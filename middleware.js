8a24ca41dc6a3a3e2449a048df74cd867a227538
sj-10ps <sps24723@gmail.com>
2026-03-01 20:58:03 +0530

    e

--- middleware.js
+++ middleware.js
@@ -1,4 +1,9 @@
-export {default} from "next-auth/middleware"
+import { withAuth } from 'next-auth/middleware'
+export default withAuth({
+    pages:{
+        signIn:'/user/profile'
+    }
+})
 export const config={
     matcher:[
         "/user/cart",
